4076fb5953d8ec55e28047fddcd2e7c78c949881
Himanshu <himanshukr.13254@gmail.com>
2022-04-22 14:57:48 +0530

    Largest number

---
+++ 2.If-else/Qno3.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a,b,c;
+    cout<<"Enter three numbers to find out the largest among them"<<endl;
+    cin>>a;
+    cin>>b;
+    cin>>c;
+    if(a>b&&a>c)
+    cout<<a<<" is the largest number"<<endl;
+    else if(b>a&&b>c) 
+    cout<<b<<" is the largest number"<<endl;
+    else
+    cout<<c<<" is the largest number"<<endl;
+    return 0;
+}
