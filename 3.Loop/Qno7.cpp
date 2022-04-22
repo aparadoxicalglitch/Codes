cfc4685e2823a0b10a777e92737e5b7a77a87f82
Himanshu <himanshukr.13254@gmail.com>
2022-04-22 20:07:12 +0530

    LCM

---
+++ 3.Loop/Qno7.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n1,n2,temp=0,hcf;
+    cout<<"Enter two numbers to find out the LCM"<<endl;
+    cin>>n1;
+    cin>>n2;
+    // if n2 is greater than n1 
+    if(n2>n1)
+    {
+        temp = n2;
+        n2 = n1;
+        n1 = temp;
+    }
+
+    for(int i=1;i<n2;i++)
+    {
+        if(n1%i==0 && n2%i==0)
+        {
+            hcf = i;
+        }
+    }
+    int lcm = (n1*n2)/hcf;
+    cout<<"LCM = "<<lcm<<endl;
+    return 0;
+}
