0d692ef9b17b64732adced214088373394e1e884
Himanshu <himanshukr.13254@gmail.com>
2022-04-24 13:07:16 +0530

    Inverted Half Pyramid

---
+++ 4.Patterns/Qno4.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+// * * * * * *
+// * * * * *
+// * * * *
+// * * *
+// * *
+// *
+
+int main()
+{
+    for (int i = 6; i > 0; i--)
+    {
+        for (int j = i; j > 0; j--)
+        {
+            cout << "*"
+                 << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
