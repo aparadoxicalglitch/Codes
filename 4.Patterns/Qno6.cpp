062d1de7b9028b40eae1f46df89627ade7ae67bf
Himanshu <himanshukr.13254@gmail.com>
2022-04-24 14:25:41 +0530

    Inverted Full Pyramid

---
+++ 4.Patterns/Qno6.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+// * * * * * *
+//  * * * * *
+//   * * * *
+//    * * *
+//     * *
+//      *
+
+int main()
+{
+    for (int i = 6; i > 0; i--)
+    {
+        for (int k = 6 - i; k > 0; k--)
+        {
+            cout << " ";
+        }
+        for (int j = i; j > 0; j--)
+        {
+            cout << "*"
+                 << " ";
+        }
+        cout << endl;
+    }
+
+    // for (int i = n; i >= 1; i--)
+    // {
+    //     for (int k = n - i; k > 0; k--)
+    //         cout << " ";
+    //     for (int j = i; j > 0; j--)
+    //         cout << "* ";
+    //     cout << endl;
+    // }
+
+            return 0;
+    }
