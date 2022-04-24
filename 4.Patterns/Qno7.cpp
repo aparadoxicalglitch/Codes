14dc14b1e532b0967ce1a9885580ebe778012461
Himanshu <himanshukr.13254@gmail.com>
2022-04-24 16:28:43 +0530

    Hollow Full Pyramid

---
+++ 4.Patterns/Qno7.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int rows;
+    cout<<"Enter the number of rows : ";
+    cin >> rows;
+    for(int i=1;i<=rows;i++)
+    {
+        for(int j=1;j<=(2*rows-1);j++)
+        {
+            if(i+j==(rows+1) || j-i == (rows-1) || i==rows)
+            {
+                cout<<"*";
+            }
+            else
+            {
+                cout<<" ";
+            }
+        }
+        cout<<endl;
+    }
+    return 0;
+}
