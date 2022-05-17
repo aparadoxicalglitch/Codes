88ab97705098cf0811498b940b792a631a156fcc
Himanshu <himanshukr.13254@gmail.com>
2022-05-17 18:16:35 +0530

    Remove all adjacent duplicates

---
+++ 0.Leetcode-easy/1047_remove_all_adjacent_duplicates.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+
+class Solution
+{
+public:
+    string removeDuplicates(string s)
+    {
+        int j = 0;
+        int i = 0;
+
+        while (i + 1 < s.length())
+        {
+            if (s[i] == s[i + 1])
+            {
+                s.erase(i, 2);
+                if (i - 1 < 0)
+                {
+                    i = 0;
+                }
+                else
+                {
+                    i--;
+                }
+            }
+            else
+            {
+                i++;
+            }
+        }
+        return s;
+    }
+};
+
+int main()
+{
+    Solution sl;
+    string s;
+    cout << "Enter a string : ";
+    cin >> s;
+
+    string ans = sl.removeDuplicates(s);
+
+    cout << "Answer : " << ans << endl;
+
+    return 0;
+}
