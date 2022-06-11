ccd15859e510e8a0ff908bf2ee83190f89d16763
Himanshu <himanshukr.13254@gmail.com>
2022-06-11 23:08:55 +0530

    Binary search using recursion

--- 0.CodeStudio/Easy/binarySearch_recursion.cpp
+++ 0.CodeStudio/Easy/binarySearch_recursion.cpp
@@ -1,25 +1,28 @@
-int solve(int *arr, int s, int e , int k ) {
+int solve(int *arr, int s, int e, int k)
+{
 
-    //base case
+    // base case
 
-    //element not found
-    if(s>e)
+    // element not found
+    if (s > e)
         return -1;
 
-    int mid = s + (e-s)/2;
+    int mid = s + (e - s) / 2;
 
-    //element found
-    if(arr[mid] == k)
+    // element found
+    if (arr[mid] == k)
         return mid;
 
-    if(arr[mid] < k) {
-        return solve(arr, mid+1, e, k);
+    if (arr[mid] < k)
+    {
+        return solve(arr, mid + 1, e, k);
     }
-    else{
-        return solve(arr, s, mid-1, k);
+    else
+    {
+        return solve(arr, s, mid - 1, k);
     }
 }
 int binarySearch(int *input, int n, int val)
 {
-    int ans = solve(input,0,n-1,val);
+    int ans = solve(input, 0, n - 1, val);
 }
