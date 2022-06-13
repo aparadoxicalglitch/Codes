12ef8d03346d886be098d0ed8b1f05d701181dda
Himanshu <himanshukr.13254@gmail.com>
2022-06-13 21:10:47 +0530

    Subsequences

--- 0.CodeStudio/Medium/subsequences.cpp
+++ 0.CodeStudio/Medium/subsequences.cpp
@@ -1,33 +1,35 @@
-#include<iostream>
-#include<bits/stdc++.h>
+#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
-void solve(vector<string>& ans, string str, string output, int i) {
-    //base case
-    if(i>=str.length()) {
-        if(output.length()>0)
-        ans.push_back(output);
-        return ;
-        }
-    
-    //exclude
-    solve(ans, str, output, i+1);
-    //include
+void solve(vector<string> &ans, string str, string output, int i)
+{
+    // base case
+    if (i >= str.length())
+    {
+        if (output.length() > 0)
+            ans.push_back(output);
+        return;
+    }
+
+    // exclude
+    solve(ans, str, output, i + 1);
+    // include
     output.push_back(str[i]);
-    solve(ans, str, output, i+1);
+    solve(ans, str, output, i + 1);
 }
 
-vector<string> subsequences(string str){
-	
-	vector<string> ans;
+vector<string> subsequences(string str)
+{
+
+    vector<string> ans;
     string output = "";
-    solve(ans,str,output,0);
+    solve(ans, str, output, 0);
     return ans;
-	
 }
 
 int main()
 {
-    
+
     return 0;
 }
