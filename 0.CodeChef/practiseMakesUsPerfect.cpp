918bd3493859f55657bb0208cc2dc9c5e1610c96
Himanshu <himanshukr.13254@gmail.com>
2023-05-17 21:42:47 +0530

    Practice makes us perfect

---
+++ 0.CodeChef/practiseMakesUsPerfect.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int w[4];
+    int s=0;
+    for(int i=1;i<=4;i++)
+    {
+        cin>>a[i];
+    }
+    for(int i=1;i<=4;i++)
+    {
+        if(w[i]>=10)
+        {
+            s++;
+        }
+    }
+    cout<<s;
+	return 0;
+}
